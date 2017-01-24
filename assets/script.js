16dba8c483d759cc8eac6be4ed548a70234e3ae1
Benjamin Arteaga <bnjmn_rtg@yahoo.com>
2017-01-23 23:12:10 -0600

    No changes

--- assets/script.js
+++ assets/script.js
@@ -12,90 +12,82 @@ var freeThrows = 0;
 var freeThrowMade = 0;
 var shotsMade = 0;
 
-$("#shotsTaken").click(function(){
-	numberOfShots++;
-	$('#shots').html(numberOfShots);
+$("#shotsTaken").click(function() {
+    numberOfShots++;
+    $('#shots').html(numberOfShots);
 });
 
-$("#twoPts").click(function(){
-	numberOfShots++;
-	shotsMade++;
-	numberOfTwos++;
-	points = points + 2 ;
-	$('#shotsMade').html(shotsMade);
-	$('#shots').html(numberOfShots);
-	$('#twoPoints').html(numberOfTwos);
-	$('#points').html(points);
+$("#twoPts").click(function() {
+    numberOfShots++;
+    shotsMade++;
+    numberOfTwos++;
+    points = points + 2;
+    $('#shotsMade').html(shotsMade);
+    $('#shots').html(numberOfShots);
+    $('#twoPoints').html(numberOfTwos);
+    $('#points').html(points);
 });
 
-$("#threePts").click(function(){
-	numberOfShots++;
-	shotsMade++;
-	numberOfThrees++;
-	points= points + 3;
-	$('#shotsMade').html(shotsMade);
-	$('#shots').html(numberOfShots);
-	$('#threePoints').html(numberOfThrees);
-	$('#points').html(points);
+$("#threePts").click(function() {
+    numberOfShots++;
+    shotsMade++;
+    numberOfThrees++;
+    points = points + 3;
+    $('#shotsMade').html(shotsMade);
+    $('#shots').html(numberOfShots);
+    $('#threePoints').html(numberOfThrees);
+    $('#points').html(points);
 });
 
-$("#freeThrowsMade").click(function(){
-	freeThrowMade++;
-	freeThrows++;
-	points++;
-	$('#missedFreeThrows').html(freeThrows);
-	$('#freeThrowPts').html(freeThrowMade);
-	$('#points').html(points);
+$("#freeThrowsMade").click(function() {
+    freeThrowMade++;
+    freeThrows++;
+    points++;
+    $('#missedFreeThrows').html(freeThrows);
+    $('#freeThrowPts').html(freeThrowMade);
+    $('#points').html(points);
 });
-$("#freeThrowsMiss").click(function(){
-	freeThrows++;
-	$('#missedFreeThrows').html(freeThrows);
+$("#freeThrowsMiss").click(function() {
+    freeThrows++;
+    $('#missedFreeThrows').html(freeThrows);
 });
-$("#blocks").click(function(){
-	blocks++;
-	$('#bl').html(blocks);
+$("#blocks").click(function() {
+    blocks++;
+    $('#bl').html(blocks);
 });
-$("#assists").click(function(){
-	assists++;
-	$('#as').html(assists);
+$("#assists").click(function() {
+    assists++;
+    $('#as').html(assists);
 });
-$("#rebounds").click(function(){
-	rebounds++;
-	$('#rb').html(rebounds);
+$("#rebounds").click(function() {
+    rebounds++;
+    $('#rb').html(rebounds);
 });
-$("#steals").click(function(){
-	steals++;
-	$('#st').html(steals);
+$("#steals").click(function() {
+    steals++;
+    $('#st').html(steals);
 });
-$("#fouls").click(function(){
-	fouls++;
-	$('#fl').html(fouls);
+$("#fouls").click(function() {
+    fouls++;
+    $('#fl').html(fouls);
 });
-$("#turnOver").click(function(){
-	turnOver++;
-	$('#to').html(turnOver);
+$("#turnOver").click(function() {
+    turnOver++;
+    $('#to').html(turnOver);
 });
-$("#percentage").click(function(){
-	var shooting = shotsMade/numberOfShots;
-	var freeThrowPercentage= freeThrowMade/freeThrows;
-	var roundedShooting = Math.round(shooting * 100);
-	var roundedFt = Math.round(freeThrowPercentage * 100);
-	if (numberOfShots === 0){
-		$('#sPercent').html(" = 0%");
+$("#percentage").click(function() {
+    var shooting = shotsMade / numberOfShots;
+    var freeThrowPercentage = freeThrowMade / freeThrows;
+    var roundedShooting = Math.round(shooting * 100);
+    var roundedFt = Math.round(freeThrowPercentage * 100);
+    if (numberOfShots === 0){
+    	$('#sPercent').html(" = 0%");
+    }
+    if (freeThrows === 0){
+    	$('#ftPercent').html(" = 0%");
+    }
+    else{
+    $('#sPercent').html(" = " + roundedShooting + "%");
+    $('#ftPercent').html(" = " + roundedFt + "%");
 	}
-	if (freeThrows === 0){
-		$('#ftPercent').html(" = 0%");
-	}
-	else{
-		$('#sPercent').html(" = " + roundedShooting + "%");
-		$('#ftPercent').html(" = " + roundedFt + "%");
-	}
 });
-
-
-	// console.log(roundedShooting);
-	// console.log(freeThrowPercentage);
-	// console.log(roundedShooting);
-	// console.log(roundedFt);
-
-
